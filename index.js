450b2f7de62118adb66928f3d1d31ccc20b894de
ferueda <fe.rueda@outlook.com>
2020-05-21 23:35:04 -0400

    project setup

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('app running on port 3000');
+});
