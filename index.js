fe37197f92048f3f85ba61150355331db05d12e7
ferueda <fe.rueda@outlook.com>
2020-05-22 22:38:57 -0400

    google oAuth initial commit work

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
+
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+  })
+);
 
 app.get('/', (req, res) => {
   res.send({ hi: 'there' });
