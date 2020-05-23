5d0245c1bfa706f11a611dab5c9ae881a7d54f68
ferueda <fe.rueda@outlook.com>
2020-05-22 23:23:21 -0400

    added passport google oauth authentication

--- index.js
+++ index.js
@@ -6,15 +6,28 @@ const keys = require('./config/keys');
 const app = express();
 
 passport.use(
-  new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token', accessToken);
+      console.log('refresh token', refreshToken);
+      console.log('profile', profile);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
   })
 );
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 3000;
 
