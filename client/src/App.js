7fc8f1c78286e02224a7001b81e2795571a021f8
ferueda <fe.rueda@outlook.com>
2020-05-24 00:17:23 -0400

    client installation initial commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <a href="http://localhost:5000/auth/google">Sign in With google</a>
+    </div>
+  );
+};
+
+export default App;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log('app running on port 3000');
+  console.log(`app running on port ${PORT}`);
 });
