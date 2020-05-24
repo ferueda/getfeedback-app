c21fef4c8ea429c2fe76d2d98d6636e53584a0fb
ferueda <fe.rueda@outlook.com>
2020-05-23 21:49:05 -0400

    added logout functionality

--- app.js
+++ app.js
@@ -32,6 +32,11 @@ app.use(passport.session());
 
 app.use('/auth', authRoutes);
 
+app.get('/api/logout', (req, res) => {
+  req.logout();
+  res.send(req.user);
+});
+
 app.get('/api/current_user', (req, res) => {
   res.send(req.user);
 });
