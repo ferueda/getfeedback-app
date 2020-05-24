51553bbe61d00822a94ba09a6e2c6d3f194ca043
ferueda <fe.rueda@outlook.com>
2020-05-24 00:36:51 -0400

    refactored app structure

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>
-      <a href="http://localhost:5000/auth/google">Sign in With google</a>
-    </div>
+    <React.Fragment>
+      <div>
+        <a href="/auth/google">Sign in With Google</a>
+      </div>
+      <div>
+        <a href="/auth/facebook">Sign in With Facebook</a>
+      </div>
+    </React.Fragment>
   );
 };
 
