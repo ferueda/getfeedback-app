82ad5dd4481ae5b5817058f9c413eab4131e44ce
ferueda <fe.rueda@outlook.com>
2020-05-28 22:45:48 -0400

    production build

--- app.js
+++ app.js
@@ -49,4 +49,13 @@ app.get('/api/logout', (req, res) => {
   res.redirect('/');
 });
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 module.exports = app;
