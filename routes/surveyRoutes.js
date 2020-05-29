fb5345e96ba2008343ad958a3cd7c88405d5b6d9
ferueda <fe.rueda@outlook.com>
2020-05-29 17:06:57 -0400

    sendgrid initial commit

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,23 @@
+const mongoose = require('mongoose');
 const surveyRoutes = require('express').Router();
 const requireLogin = require('../middleware/requireLogin');
+const requireCredits = require('../middleware/requireCredits');
 
-surveyRoutes.post('/', requireLogin, async (req, res) => {});
+const Survey = mongoose.model('Survey');
+
+surveyRoutes.post('/', requireLogin, requireCredits, async (req, res) => {
+  const { title, subject, body, recipients } = req.body;
+
+  const survey = new Survey({
+    title,
+    subject,
+    body,
+    recipients: recipients.split(',').map((email) => ({ email: email.trim() })),
+    _user: req.user.id,
+    dateSent: Date.now(),
+  });
+
+  survey.save();
+});
 
 module.exports = surveyRoutes;
