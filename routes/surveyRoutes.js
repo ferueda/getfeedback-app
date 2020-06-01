871154a19874e59faef150fac683ec835913ef67
ferueda <fe.rueda@outlook.com>
2020-05-31 22:38:53 -0400

    added the Mailer helper

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,9 +1,11 @@
+const mongoose = require('mongoose');
 const surveyRoutes = require('express').Router();
 const requireLogin = require('../middleware/requireLogin');
 const requireCredits = require('../middleware/requireCredits');
-require('../models/Survey');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
-// const Survey = mongoose.model('Survey');
+const Survey = mongoose.model('surveys');
 
 surveyRoutes.post('/', requireLogin, requireCredits, async (req, res) => {
   const { title, subject, body, recipients } = req.body;
@@ -17,7 +19,9 @@ surveyRoutes.post('/', requireLogin, requireCredits, async (req, res) => {
     dateSent: Date.now(),
   });
 
-  survey.save();
+  // survey.save();
+
+  const mailer = new Mailer(survey, surveyTemplate(survey));
 });
 
 module.exports = surveyRoutes;
---
+++ services/Mailer.js
@@ -0,0 +1,40 @@
+const sendgrid = require('sendgrid');
+const helper = sendgrid.mail;
+const keys = require('../config/keys');
+
+class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
+
+    this.from_email = new helper.Email('fe.rueda@hotmail.com');
+    this.subject = subject;
+    this.body = new helper.Content('text/html', content);
+    this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
+  }
+
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => new helper.Email(email));
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
+
+  addRecipients() {
+    const personalize = new helper.Personalization();
+    this.recipients.forEach((recipient) => {
+      personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
+  }
+}
+
+module.exports = Mailer;
