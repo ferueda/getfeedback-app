3068984ab62376a7a10a3a073c3a6ebb0bfd2116
ferueda <fe.rueda@outlook.com>
2020-06-03 20:56:13 -0400

    added the get route that handles the sendgrid webhook and updates the Surveys data in mongoDB

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,7 @@
+const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url');
+
 const mongoose = require('mongoose');
 const surveyRoutes = require('express').Router();
 const requireLogin = require('../middleware/requireLogin');
@@ -36,4 +40,44 @@ surveyRoutes.post('/', requireLogin, requireCredits, async (req, res) => {
   }
 });
 
+
+surveyRoutes.post('/webhooks', (req, res) => {
+  const p = new Path('/api/surveys/:surveyId/:choice');
+
+  _.chain(req.body)
+    .map(({ url, email }) => {
+      const match = p.test(new URL(url).pathname);
+
+      if (match) {
+        const { surveyId, choice } = match;
+
+        return {
+          email,
+          surveyId,
+          choice,
+        };
+      }
+    })
+    .compact()
+    .uniqBy('email', 'surveyId')
+    .each(({ surveyId, email, choice }) => {
+      Survey.updateOne(
+        {
+          _id: surveyId,
+          recipients: {
+            $elemMatch: {
+              email: email,
+              responded: false,
+            },
+          },
+        },
+        {
+          $inc: { [choice]: 1 },
+          $set: { 'recipients.$.responded': true },
+          lastResponded: new Date(),
+        }
+      ).exec();
+    })
+    .value();
+});
 module.exports = surveyRoutes;
